48874276d86e5484fb9d8ebb189c20b00643560c
faqih996 <faqih.syakir11@gmail.com>
2021-10-27 13:09:36 +0700

    pertama upload

---
+++ javascript-bwa/vanillajs/dist/array.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var namaMantan = ["kiki", "ayu", "ezra"]; //pakai [0/1/2] untuk melihat salah satu. sama dengan id di php
+//atau menggunakan (namaMantan.lenght) untuk melihat jumlah mantan
+//console.log(namaMantan);
+
+namaMantan.forEach(function (item, index, array) {
+  console.log(item, index, array);
+});
+namaMantan.push('harutya'); //duplikat array 
+
+var namaMantanLama = namaMantan.slice();
+namaMantan.pop();
+console.log("nama mantan: ", namaMantan);
+console.log("Nama Mantan Lama:", namaMantanLama); //HAPUS DATA TERAKHIR
+//namaMantan.pop();
+//console.log(namaMantan);
+//HAPUS DATA PERTAMA
+//namaMantan.shift();
+//console.log(namaMantan);
+//menghapus nama tertentu dengan cara 1
+//namaMantan.splice(namaMantan.indexOf("kiki"), 1)
+//console.log(namaMantan);
+//mencari dia ini mantan keberapa
+//console.log(namaMantan.indexOf("ezra") + 1);
+//menghapus nama tertentu
+//const indexKiki = namaMantan.indexOf("kiki");
+//console.log(indexKiki);
+//namaMantan.splice(indexKiki, 1);
+//console.log(namaMantan);
---
+++ javascript-bwa/vanillajs/dist/new.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//dasar
+//var iceCream = "5"
+//console.log(iceCream)
+//menggunakan variable dengan let
+// let iceCream = "5"
+//console.log(iceCream)
+//menggunakan const maka nilai variable harus sama
+//const iceCream = "5" 
+//console.log(iceCream)
+//var iceCream = "5" 
+//console.log(iceCream)
+//type data hanya ada bolean, array, string, int
+//operator =, ==, ===, >, <
+//const iceCream = 7 % 3;
+//if(iceCream ==2)
+//console.log(iceCream);
+//else
+//console.log("ice cream tidak sama dengan 2")
+// logical separator &&, ||, !
+//const sayaSukaEsKrim = false;
+//const sayaSukaHamburger = false;
+//if (!sayaSukaEsKrim && sayaSukaHamburger)
+//console.log('saya suka es krim dan hamburger');
+//ini pake !
+//    console.log(!sayaSukaEsKrim)
+//const sayaSukaEsKrim = "tentu"
+//const sayaSukaHamburger = "gak akan nolak"
+//console.log(sayaSukaEsKrim + " " + sayaSukaHamburger);
+var sayaSukaEsKrim = "tentu";
+var sayaSukaHamburger = "gak akan nolak";
+console.log("".concat(sayaSukaEsKrim, " ").concat(sayaSukaHamburger));
