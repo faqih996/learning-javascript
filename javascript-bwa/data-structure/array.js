9d1b29afb2cde7dde2989a9db049d654a6b64585
faqih996 <faqih.syakir11@gmail.com>
2021-10-29 14:34:38 +0700

    nextjs

---
+++ javascript-bwa/data-structure/array.js
@@ -0,0 +1,5 @@
+let fruits = ["apple", "manggo", "orange", "water melon"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
---
+++ javascript-bwa/data-structure/dist/array.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var fruits = ["apple", "manggo", "orange", "water melon"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
